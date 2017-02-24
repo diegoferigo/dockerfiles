11f0b1703cacf6df24ac9085b5a4ac36fed72776
Diego Ferigo <dgferigo@gmail.com>
2017-02-24 11:12:34 +0100

    Ported 883dbca18f42b13abea573f178d47b6aeb75d40e to the other images
    [ROS][Tools][YARP]

--- YARP/entrypoint.sh
+++ YARP/entrypoint.sh
@@ -7,22 +7,37 @@ USER_GID=${USER_GID:-1000}
 USERNAME=${USERNAME:-docker}
 
 create_user() {
-	# If the home folder exists, copy inside the default files of a home directory
-	if [ -d /home/${USERNAME} ] ; then
-		chown ${USER_UID}:${USER_GID} /home/${USERNAME}
-		install -m 644 -g ${USERNAME} -o ${USERNAME} /etc/skel/.bashrc /home/${USERNAME}
-		install -m 644 -g ${USERNAME} -o ${USERNAME} /etc/skel/.bash_logout /home/${USERNAME}
-		install -m 644 -g ${USERNAME} -o ${USERNAME} /etc/skel/.profile /home/${USERNAME}
-	fi
+	# If the home folder exists, set a flag.
+	# Creating the user during container initialization often is anticipated
+	# by the mount of a docker volume. In this case the home directory is already
+	# present in the file system and adduser skips by default the copy of the
+	# configuration files
+	HOME_FOLDER_EXISTS=0
+	if [ -d /home/$USERNAME ] ; then HOME_FOLDER_EXISTS=1 ; fi
 
 	# Create a group with USER_GID
 	if ! getent group ${USERNAME} >/dev/null; then
+		echo "Creating ${USERNAME} group"
 		groupadd -f -g ${USER_GID} ${USERNAME} 2> /dev/null
 	fi
 
 	# Create a user with USER_UID
 	if ! getent passwd ${USERNAME} >/dev/null; then
-		adduser --disabled-login --uid ${USER_UID} --gid ${USER_GID} --gecos 'Workspace' ${USERNAME}
+		echo "Creating ${USERNAME} user"
+		adduser --quiet \
+		        --disabled-login \
+				  --uid ${USER_UID} \
+				  --gid ${USER_GID} \
+				  --gecos 'Workspace' \
+				  ${USERNAME}
+	fi
+
+	# If configuration files have not been copied, do it manually
+	if [ HOME_FOLDER_EXISTS ] ; then
+		chown ${USER_UID}:${USER_GID} /home/${USERNAME}
+		install -m 644 -g ${USERNAME} -o ${USERNAME} /etc/skel/.bashrc /home/${USERNAME}
+		install -m 644 -g ${USERNAME} -o ${USERNAME} /etc/skel/.bash_logout /home/${USERNAME}
+		install -m 644 -g ${USERNAME} -o ${USERNAME} /etc/skel/.profile /home/${USERNAME}
 	fi
 }
 
